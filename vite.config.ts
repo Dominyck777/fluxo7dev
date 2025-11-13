59b44cb1c21f5356a4a7264da344b8ff5218d5f7
Kallew Felip <kallewfelip100@gmail.com>
2025-11-13 15:10:38 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    historyApiFallback: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
       },
     },
   },
